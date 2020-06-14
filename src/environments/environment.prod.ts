c25f0a1d7094a1e3f1b31af2b21dbdac457825c8
varunkondreddy <45779365+varunkondreddy@users.noreply.github.com>
2020-06-14 12:46:35 +0530

    second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyA-vtzQdKiOu47ra4w-hbeI7rOTWztr9_s",
+    authDomain: "notification-1f575.firebaseapp.com",
+    databaseURL: "https://notification-1f575.firebaseio.com",
+    projectId: "notification-1f575",
+    storageBucket: "notification-1f575.appspot.com",
+    messagingSenderId: "1031857244742",
+    appId: "1:1031857244742:web:b5ae81b942d4a41d941847",
+    measurementId: "G-2JT96B09ZG"
+  }
 };
